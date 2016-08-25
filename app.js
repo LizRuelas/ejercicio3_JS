6f58819b0172a0acc65f3a57509003c72414dbb8
LizRuelas <liz.ruelas.borda@gmail.com>
2016-08-25 13:50:21 -0700

    modificando app.js

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ var horas;
 var pago;
 var sueldo;
          
-horas=prompt("Ingrese las horas de trabajo : ","");
-pago=prompt("Ingrese el monto de pago : ","");
+horas=prompt("Ingrese las horas de trabajo : ");
+pago=prompt("Ingrese el monto de pago : ");
 sueldo= (parseInt(horas) * parseInt(pago));
 
 window.alert("El sueldo del trabajador es : " + sueldo);
